6c16de8f1e7d4cc030564eb16038d5191ecb8f7b
HawerPL <hubertsob217@gmail.com>
2022-04-17 18:37:17 +0200

    [hs] Dodanie nowych komponentów (navbar, hompage, main-section)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomepageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
